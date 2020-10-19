00f52f59c043fd56016aa71002b4f11fac44567a
misko183 <rtmike183@gmail.com>
2020-10-19 09:56:35 +0200

    I changed, to. and I still Implement the Format method (double [] numbers)

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace Open_Lab_02._02
 {
@@ -6,7 +7,18 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+            string message = "";
+            foreach (double i in numbers)
+
+            {
+                message = message + string.Format("{0:0.00} ", i);
+            }
+
+            message = message.Replace("," , ".");
+            message = message.Replace("," , ".");
+            message = message.Trim();
+            Console.Write(message);
+
         }
     }
 }
